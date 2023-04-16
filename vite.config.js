dae231fd442e8d4ab020ba2f6e65016d48984019
Emir Halıcı <emir.halici1@gmail.com>
2023-04-16 22:39:12 +0300

    fix: remove base from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/card-game-React/',
+  // base: '/card-game-React/',
   assetsDir: 'public',
 });
