abea8134337442fa648cbc3f7d3975ab671c8e51
Emir Halıcı <emir.halici1@gmail.com>
2023-04-16 22:43:15 +0300

    fix: change assets dir

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/card-game-React/',
-  assetsDir: 'public',
+  base: '/card-game-React/',
+  assetsDir: 'assets',
+  build: {
+    outDir: 'gh-pages',
+  },
 });
